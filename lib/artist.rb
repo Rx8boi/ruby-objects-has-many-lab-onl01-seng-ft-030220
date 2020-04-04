8411f72c8dd1a38c3cf7d95d80b8b1aafbf71215
Charles Butler <cdbutle@outlook.com>
2020-04-04 22:20:57 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,29 @@
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :songs
 
-def initialize(name)
-  @name = name
-end
+  @@song_count = 1
 
-def songs
-  song.all.select {|song| song.artist == self }
-end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-def add_song(song)
-  song.artist = self
-end
-def add_song_by_name(name)
-  song = Song.new(name)
-  add_song(song)
-end
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
 
-def self.song_count
-  song.all.length
-end
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
 
-
-
+  def self.song_count
+    @@song_count
+  end
 
 end
