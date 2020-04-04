51d8b060a33da08d1f1213bdc04bf5402af91c20
Charles Butler <cdbutle@outlook.com>
2020-04-04 21:57:57 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,12 @@
 class Author
 
   attr_accessor :name, :posts
-  @@post_count = 1
+  @@post_count = 0
 
   def initialize(name)
     @name = name
     @posts = []
+    @@post_count += 1
   end
 
   def add_post(post)
